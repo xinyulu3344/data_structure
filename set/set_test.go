d3b65fb09ec8e5402d997bb28116d999221b3f9d
xinyulu <xinyulu3344@163.com>
2021-05-31 13:10:59 +0800

    集合实现

--- set/set_test.go
+++ set/set_test.go
@@ -1,11 +1,22 @@
 /**
   @author: xinyulu
   @date: 2021/1/31 20:39
-  @note: 
+  @note:
 **/
 package set
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSet(t *testing.T) {
+    listSet := NewListSet()
+    listSet.Add(10)
+    listSet.Add(11)
+    listSet.Add(11)
+    listSet.Add(12)
+    listSet.traversal(func(e interface{}) {
+        fmt.Println(e)
+    })
 }
