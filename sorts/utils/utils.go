979c3c7bdb184c85419126290d987cbafff71131
xinyulu <xinyulu3344@163.com>
2021-11-18 00:05:29 +0800

    feat:  查找元素在有序切片中的索引 feat:  查找元素在有序数组中的插入位置

--- sorts/utils/utils.go
+++ sorts/utils/utils.go
@@ -36,3 +36,44 @@ func IsDesSortedInts(elements []int) bool {
     }
     return true
 }
+
+// IndexOfInt
+// elements: 元素数组
+// v: 待查找元素
+// return: 待查找元素在数组中的索引
+func IndexOfInt(elements []int, v int) int {
+    if elements == nil || len(elements) == 0 {
+        return -1
+    }
+    begin := 0
+    end := len(elements)
+    for begin < end {
+        mid := (end + begin) >> 1
+        if v < elements[mid] {
+            end = mid
+        } else if v > elements[mid] {
+            begin = mid + 1
+        } else {
+            return mid
+        }
+    }
+    return -1
+}
+
+// Search 查找v在有序slice elements中的插入位置
+func Search(elements []int, v int) int {
+    if elements == nil || len(elements) == 0 {
+        return -1
+    }
+    begin := 0
+    end := len(elements)
+    for begin < end {
+        mid := (end + begin) >> 1
+        if v < elements[mid] {
+            end = mid
+        } else {
+            begin = mid + 1
+        }
+    }
+    return begin
+}
---
+++ sorts/utils/utils_test.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+    "testing"
+)
+
+func TestIndexOfInt(t *testing.T) {
+    orderInts := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+    t.Log(IndexOfInt(orderInts, 3))
+}
+
+func TestSearch(t *testing.T) {
+    orderInts := []int{2, 4, 8, 8, 8, 12, 14}
+    t.Log(Search(orderInts, 5) == 2)
+    t.Log(Search(orderInts, 1) == 0)
+    t.Log(Search(orderInts, 15) == 7)
+    t.Log(Search(orderInts, 8) == 5)
+}
