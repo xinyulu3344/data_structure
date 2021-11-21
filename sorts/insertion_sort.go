939db368d05e81755851fbab17c6a811aa999518
xinyulu <xinyulu3344@163.com>
2021-11-21 14:18:57 +0800

    重构插入排序

--- sorts/insertion_sort.go
+++ sorts/insertion_sort.go
@@ -1,183 +1,93 @@
 package sorts
 
 type InsertionSort struct {
-    elements []interface{}
-    elementsInt []int
-    cmp Comparator
+    //elements []interface{}
+    ascend bool
+    elements Interface
     cmpCount int
     swapCount int
 }
 
-func NewInsertionSort() *InsertionSort {
-    return &InsertionSort{}
-}
-
-func NewInsertionSortWithComparator(cmp Comparator) *InsertionSort {
+func NewInsertionSort(ascend bool) *InsertionSort {
     return &InsertionSort{
-        cmp: cmp,
+        ascend: ascend,
     }
 }
 
-// 二分法插入排序，优化插入位置查找
-func (is *InsertionSort) AsSortInt(elements []int) {
-   is.elementsInt = elements
-   length := len(elements)
-   for begin := 1; begin < length; begin++ {
-       is.insertInt(begin, is.searchInt(begin))
-   }
+// SetAscend 设置是否升序排序
+func (is *InsertionSort) SetAscend(ascend bool) {
+    is.ascend = ascend
 }
 
-// 将待插入元素不断和前面的元素比较，如果比前面元素小，就将前面的元素往后挪，直到比前面元素大为止
-func (is *InsertionSort) AsSortIntMove(elements []int) {
+// SortInt 二分查找法给整数排序
+func (is *InsertionSort) SortInt(elements []int) {
+    is.Sort(IntSlice(elements))
+}
+
+// SortSwapInt 交换法给整数排序
+func (is *InsertionSort) SortSwapInt(elements []int) {
+    is.SortSwap(IntSlice(elements))
+}
+
+// Sort 二分查找法排序
+func (is *InsertionSort) Sort(elements Interface) {
     is.cmpCount = 0
     is.swapCount = 0
-    is.elementsInt = elements
-    length := len(elements)
-    // 遍历元素切片
-    for begin := 1; begin < length; begin++ {
-        // 保存待插入元素索引
-        cur := begin
-        // 保存待插入元素
-        v := is.elementsInt[cur]
-        // 循环比较待插入元素和前面的元素的大小，如果比前面的小，就将前面的元素往后挪
-        for cur > 0 && is.compareElementsInt(v, is.elementsInt[cur - 1]) < 0 {
-            is.elementsInt[cur] = is.elementsInt[cur - 1]
-            cur--
-        }
-        // 已经找到合适的位置插入元素
-        is.elementsInt[cur] = v
-    }
-}
-
-// 将待插入元素不断和前面的元素交换位置，直到比前面的元素大为止
-func (is *InsertionSort) AsSortIntSwap(elements []int) {
-    is.cmpCount = 0
-    is.swapCount = 0
-    is.elementsInt = elements
-    length := len(elements)
-    // 遍历元素切片
-    for begin := 1; begin < length; begin++ {
-        // 记录待插入元素索引
-        cur := begin
-        // 循环比较待插入元素和前面的元素的大小，如果比前面的元素小，就和前面的元素交换位置
-        for cur > 0 && is.compareInt(cur, cur - 1) < 0 {
-            is.swapInt(cur, cur - 1)
-            cur--
-        }
-    }
-}
-
-// 二分法插入排序，优化插入位置查找
-func (is *InsertionSort) AsSort(elements []interface{}) {
     is.elements = elements
-    length := len(elements)
+    length := elements.Len()
     for begin := 1; begin < length; begin++ {
+        // 将查找到的元素，插入到begin位置
         is.insert(begin, is.search(begin))
     }
 }
 
-func (is *InsertionSort) AsSortMove(elements []interface{}) {
+// SortSwap 交换法排序
+func (is *InsertionSort) SortSwap(elements Interface) {
     is.cmpCount = 0
     is.swapCount = 0
     is.elements = elements
-    length := len(elements)
-    for begin := 1; begin < length; begin++ {
-        cur := begin
-        v := is.elements[cur]
-        for cur > 0 && is.compareElements(v, is.elements[cur - 1]) < 0 {
-            is.elements[cur] = is.elements[cur - 1]
-            cur--
+    length := elements.Len()
+    if is.ascend {
+        for begin := 1; begin < length; begin++ {
+            cur := begin
+            for cur > 0 && is.compare(cur, cur - 1) < 0 {
+                is.swap(cur, cur - 1)
+                cur--
+            }
         }
-        is.elements[cur] = v
-    }
-}
-
-func (is *InsertionSort) AsSortSwap(elements []interface{}) {
-    is.cmpCount = 0
-    is.swapCount = 0
-    is.elements = elements
-    length := len(elements)
-    for begin := 1; begin < length; begin++ {
-        cur := begin
-        for cur > 0 && is.compare(cur, cur - 1) < 0 {
-            is.swap(cur, cur - 1)
-            cur--
+    } else {
+        for begin := 1; begin < length; begin++ {
+            cur := begin
+            for cur > 0 && is.compare(cur, cur - 1) > 0 {
+                is.swap(cur, cur - 1)
+                cur--
+            }
         }
     }
 }
 
+// swap 交换指定索引元素的位置
 func (is *InsertionSort) swap(index1, index2 int) {
     is.swapCount++
-    tmp := is.elements[index1]
-    is.elements[index1] = is.elements[index2]
-    is.elements[index2] = tmp
+    is.elements.Swap(index1, index2)
 }
 
-func (is *InsertionSort) swapInt(index1, index2 int) {
-    is.swapCount++
-    tmp := is.elementsInt[index1]
-    is.elementsInt[index1] = is.elementsInt[index2]
-    is.elementsInt[index2] = tmp
-}
-
+// compare 比较指定索引元素的大小
 func (is *InsertionSort) compare(index1, index2 int) int {
     is.cmpCount++
-    if is.cmp != nil {
-        return is.cmp.CompareTo(is.elements[index1], is.elements[index2])
-    }
-    return is.elements[index1].(Comparable).Compare(is.elements[index2])
+    return is.elements.Compare(index1, index2)
 }
 
-func (is *InsertionSort) compareElements(e1, e2 interface{}) int {
-    is.cmpCount++
-    if is.cmp != nil {
-        return is.cmp.CompareTo(e1, e2)
-    }
-    return e1.(Comparable).Compare(e2)
-}
 
-func (is *InsertionSort) compareInt(index1, index2 int) int {
-    is.cmpCount++
-    return is.elementsInt[index1] - is.elementsInt[index2]
-}
-
-func (is *InsertionSort) compareElementsInt(e1, e2 int) int {
-    is.cmpCount++
-    return e1 - e2
-}
-
-// 利用二分搜索，找到 index 位置元素的待插入位置
-// 已经排好序 slice 的区间范围是 [0, index)
-func (is *InsertionSort) searchInt(index int) int {
-    begin := 0
-    end := index
-    for begin < end {
-        mid := (begin + end) >> 1
-        if is.compareInt(index, mid) < 0 {
-            end = mid
-        } else {
-            begin = mid + 1
-        }
-    }
-    return begin
-}
-
-// 将 dest 位置的元素插入到 dest 位置
-func (is *InsertionSort) insertInt(src int, dest int) {
-    v := is.elementsInt[src]
-    for i := src; i > dest; i-- {
-        is.elementsInt[i] = is.elementsInt[i - 1]
-    }
-    is.elementsInt[dest] = v
-}
-
-// 将 dest 位置的元素插入到 dest 位置
+// 将 src 位置的元素插入到 dest 位置
 func (is *InsertionSort) insert(src int, dest int) {
-    v := is.elements[src]
-    for i := src; i > dest; i-- {
-        is.elements[i] = is.elements[i - 1]
+    // 先交换待插入元素和插入位置元素的位置
+    is.swap(src, dest)
+    lastIndex := dest + 1
+    // 将交换后的插入位置元素，和前面的元素依次交换位置，来实现元素后移
+    for i := src; i > lastIndex; i-- {
+        is.swap(i, i - 1)
     }
-    is.elements[dest] = v
 }
 
 // 利用二分搜索，找到 index 位置元素的待插入位置
@@ -185,12 +95,24 @@ func (is *InsertionSort) insert(src int, dest int) {
 func (is *InsertionSort) search(index int) int {
     begin := 0
     end := index
-    for begin < end {
-        mid := (begin + end) >> 1
-        if is.compare(index, mid) < 0 {
-            end = mid
-        } else {
-            begin = mid + 1
+    if is.ascend {
+        for begin < end {
+            mid := (begin + end) >> 1
+            if is.compare(index, mid) < 0 {
+                end = mid
+            } else {
+                begin = mid + 1
+            }
+        }
+        
+    } else {
+        for begin < end {
+            mid := (begin + end) >> 1
+            if is.compare(index, mid) > 0 {
+                end = mid
+            } else {
+                begin = mid + 1
+            }
         }
     }
     return begin
--- sorts/insertion_sort_test.go
+++ sorts/insertion_sort_test.go
@@ -9,144 +9,128 @@ func TestInsertionSortAsSortInt(t *testing.T) {
     randInts := rand.Perm(10000)
     t.Log(randInts)
     
-    is := NewInsertionSort()
-    is.AsSortInt(randInts)
+    is := NewInsertionSort(true)
+    is.SortInt(randInts)
     
     t.Log(randInts)
     t.Log("cmpCount: ", is.cmpCount)
     t.Log("swapCount: ", is.swapCount)
-    t.Log(IsAsSortedInts(randInts))
-}
-
-func TestInsertionSortAsSortIntMove(t *testing.T) {
-    randInts := rand.Perm(10000)
-    t.Log(randInts)
-    
-    is := NewInsertionSort()
-    is.AsSortIntMove(randInts)
-    
-    t.Log(randInts)
-    t.Log("cmpCount: ", is.cmpCount)
-    t.Log("swapCount: ", is.swapCount)
-    t.Log(IsAsSortedInts(randInts))
+    t.Log(IntsAreAsSorted(randInts))
 }
 
 func TestInsertionSortAsSortIntSwap(t *testing.T) {
     randInts := rand.Perm(10000)
     t.Log(randInts)
     
-    is := NewInsertionSort()
-    is.AsSortIntSwap(randInts)
+    is := NewInsertionSort(true)
+    is.SortSwapInt(randInts)
     
     t.Log(randInts)
     t.Log("cmpCount: ", is.cmpCount)
     t.Log("swapCount: ", is.swapCount)
-    t.Log(IsAsSortedInts(randInts))
+    t.Log(IntsAreAsSorted(randInts))
 }
 
 func BenchmarkInsertionSortAsSortInt(b *testing.B) {
-    is := NewInsertionSort()
+    is := NewInsertionSort(true)
     for i := 0; i < b.N; i++ {
         randInts := rand.Perm(10000)
-        is.AsSortInt(randInts)
-    }
-}
-
-func BenchmarkInsertionSortAsSortIntMove(b *testing.B) {
-    is := NewInsertionSort()
-    for i := 0; i < b.N; i++ {
-        randInts := rand.Perm(10000)
-        is.AsSortIntMove(randInts)
+        is.SortInt(randInts)
     }
 }
 
 func BenchmarkInsertionSortAsSortIntSwap(b *testing.B) {
-    is := NewInsertionSort()
+    is := NewInsertionSort(true)
     for i := 0; i < b.N; i++ {
         randInts := rand.Perm(10000)
-        is.AsSortIntSwap(randInts)
+        is.SortSwapInt(randInts)
     }
 }
 
 func TestInsertionSortAsSort(t *testing.T) {
-    //s1 := []interface{}{Integer(47), Integer(9), Integer(38), Integer(61), Integer(73), Integer(59), Integer(52), Integer(56), Integer(27), Integer(90)}
-    s1 := getInterfaceRands(10000)
+    s1 := NewByAge(10000)
     t.Log(s1)
-    is := NewInsertionSort()
-    is.AsSort(s1)
+    is := NewInsertionSort(true)
+    is.Sort(s1)
     t.Log(s1)
     t.Log("cmpCount: ", is.cmpCount)
     t.Log("swapCount: ", is.swapCount)
+    t.Log(IsAsSorted(s1))
 }
 
 func BenchmarkInsertionSortAsSort(b *testing.B) {
-    is := NewInsertionSort()
+    is := NewInsertionSort(true)
     for i := 0; i < b.N; i++ {
-        //s1 := []interface{}{Integer(47), Integer(9), Integer(38), Integer(61), Integer(73), Integer(59), Integer(52), Integer(56), Integer(27), Integer(90)}
-        s1 := getInterfaceRands(10000)
-        is.AsSort(s1)
+        s1 := NewByAge(10000)
+        is.Sort(s1)
     }
 }
 
-func TestInsertionSortAsSortMove(t *testing.T) {
-    //s1 := []interface{}{Integer(47), Integer(9), Integer(38), Integer(61), Integer(73), Integer(59), Integer(52), Integer(56), Integer(27), Integer(90)}
-    s1 := getInterfaceRands(10000)
-    t.Log(s1)
-    is := NewInsertionSort()
-    is.AsSortMove(s1)
-    t.Log(s1)
-    t.Log("cmpCount: ", is.cmpCount)
-    t.Log("swapCount: ", is.swapCount)
-}
-
-func BenchmarkInsertionSortAsSortMove(b *testing.B) {
-    is := NewInsertionSort()
-    for i := 0; i < b.N; i++ {
-        //s1 := []interface{}{Integer(47), Integer(9), Integer(38), Integer(61), Integer(73), Integer(59), Integer(52), Integer(56), Integer(27), Integer(90)}
-        s1 := getInterfaceRands(10000)
-        is.AsSortMove(s1)
-    }
-}
 
 func TestInsertionSortAsSortSwap(t *testing.T) {
-    //s1 := []interface{}{Integer(47), Integer(9), Integer(38), Integer(61), Integer(73), Integer(59), Integer(52), Integer(56), Integer(27), Integer(90)}
-    s1 := getInterfaceRands(10000)
+    s1 := NewByAge(10000)
     t.Log(s1)
-    is := NewInsertionSort()
-    is.AsSortSwap(s1)
+    is := NewInsertionSort(true)
+    is.SortSwap(s1)
     t.Log(s1)
     t.Log("cmpCount: ", is.cmpCount)
     t.Log("swapCount: ", is.swapCount)
+    t.Log(IsAsSorted(s1))
 }
 
 func BenchmarkInsertionSortAsSortSwap(b *testing.B) {
-    is := NewInsertionSort()
+    is := NewInsertionSort(true)
     for i := 0; i < b.N; i++ {
-        //s1 := []interface{}{Integer(47), Integer(9), Integer(38), Integer(61), Integer(73), Integer(59), Integer(52), Integer(56), Integer(27), Integer(90)}
-        s1 := getInterfaceRands(10000)
-        is.AsSortSwap(s1)
+        s1 := NewByAge(10000)
+        is.SortSwap(s1)
     }
 }
 
-func TestNewInsertionSortWithComparator(t *testing.T) {
-    s1 := []interface{}{47, 9, 38, 61, 73, 59, 52, 56, 27, 90}
-    var intComparator *IntComparator
-    is := NewInsertionSortWithComparator(intComparator)
+func TestInsertionSortDsSortInt(t *testing.T) {
+    randInts := rand.Perm(10000)
+    t.Log(randInts)
+    
+    is := NewInsertionSort(false)
+    is.SortInt(randInts)
+    
+    t.Log(randInts)
+    t.Log("cmpCount: ", is.cmpCount)
+    t.Log("swapCount: ", is.swapCount)
+    t.Log(IntsAreDsSorted(randInts))
+}
+
+func TestInsertionSortDsSortIntSwap(t *testing.T) {
+    randInts := rand.Perm(10000)
+    t.Log(randInts)
+    
+    is := NewInsertionSort(false)
+    is.SortSwapInt(randInts)
+    
+    t.Log(randInts)
+    t.Log("cmpCount: ", is.cmpCount)
+    t.Log("swapCount: ", is.swapCount)
+    t.Log(IntsAreDsSorted(randInts))
+}
+
+func TestInsertionSortDsSort(t *testing.T) {
+    s1 := NewByAge(10000)
     t.Log(s1)
-    is.AsSort(s1)
+    is := NewInsertionSort(false)
+    is.Sort(s1)
     t.Log(s1)
     t.Log("cmpCount: ", is.cmpCount)
     t.Log("swapCount: ", is.swapCount)
+    t.Log(IsDsSorted(s1))
 }
 
-func getInterfaceRands(count int) []interface{} {
-    ints := make([]interface{}, count)
-    for i := 0; i < count; i++ {
-        ints[i] = Integer(rand.Intn(count))
-    }
-    return ints
-}
+func TestInsertionSortDsSortSwap(t *testing.T) {
+    s1 := NewByAge(10000)
+    t.Log(s1)
+    is := NewInsertionSort(false)
+    is.SortSwap(s1)
+    t.Log(s1)
+    t.Log("cmpCount: ", is.cmpCount)
+    t.Log("swapCount: ", is.swapCount)
+    t.Log(IsDsSorted(s1))
 
-func TestGetInterfaceRands(t *testing.T) {
-    t.Log(getInterfaceRands(10000))
 }
