e19d45768caae8ff7e891c6036ab7360b63ac037
xinyulu3344 <xinyulu3344@163.com>
2021-11-29 16:58:57 +0800

    feat: 计数排序支持对象排序

--- sorts/counting_sort.go
+++ sorts/counting_sort.go
@@ -1,30 +1,38 @@
 package sorts
 
 type CountingSort struct {
-    elements []int
+    elementsInt []int
+    elements Elements
 }
 
 func NewCountingSort() *CountingSort {
     return &CountingSort{}
 }
 
-// SortInt
-// 最好、最坏、平均时间复杂度: O(n+k)
-// 空间复杂度: O(n+k)
-// k 是整数的取值范围
-// 属于稳定排序
-func (cs *CountingSort) SortInt(elements []int) {
+type Element interface {
+    GetNum() int
+}
+
+type Elements []Element
+
+func (e Elements) GetElement(index int) Element {
+    return e[index]
+}
+
+// Sort
+// 支持对象排序
+func (cs *CountingSort) Sort(elements Elements) {
     cs.elements = elements
     length := len(cs.elements)
-    max := cs.elements[0]
-    min := cs.elements[0]
+    max := cs.elements[0].GetNum()
+    min := cs.elements[0].GetNum()
     // 找出最大、小值
     for i := 1; i < length; i++ {
-        if cs.elements[i] > max {
-            max = cs.elements[i]
+        if cs.elements[i].GetNum() > max {
+            max = cs.elements[i].GetNum()
         }
-        if cs.elements[i] < min {
-            min = cs.elements[i]
+        if cs.elements[i].GetNum() < min {
+            min = cs.elements[i].GetNum()
         }
     }
     
@@ -33,7 +41,50 @@ func (cs *CountingSort) SortInt(elements []int) {
     countsLength := len(counts)
     // 统计每个整数出现的次数
     for i := 0; i < length; i++ {
-        counts[cs.elements[i] - min]++
+        counts[cs.elements[i].GetNum() - min]++
+    }
+    // 累加次数
+    for i := 1; i < countsLength; i++ {
+        counts[i] += counts[i - 1]
+    }
+    
+    // 从后往前遍历元素，将它放到有序数组中的合适位置
+    newElements := make(Elements, length)
+    for i := length - 1; i >= 0; i-- {
+        counts[cs.elements[i].GetNum() - min]--
+        newElements[counts[cs.elements[i].GetNum() - min]] = cs.elements[i]
+    }
+    for i := 0; i < length; i++ {
+        cs.elements[i] =  newElements[i]
+    }
+}
+
+// SortInt
+// 最好、最坏、平均时间复杂度: O(n+k)
+// 空间复杂度: O(n+k)
+// k 是整数的取值范围
+// 属于稳定排序
+func (cs *CountingSort) SortInt(elements []int) {
+    cs.elementsInt = elements
+    length := len(cs.elements)
+    max := cs.elementsInt[0]
+    min := cs.elementsInt[0]
+    // 找出最大、小值
+    for i := 1; i < length; i++ {
+        if cs.elementsInt[i] > max {
+            max = cs.elementsInt[i]
+        }
+        if cs.elementsInt[i] < min {
+            min = cs.elementsInt[i]
+        }
+    }
+    
+    // 开辟内存空间，存储每个整数出现的次数
+    counts := make([]int, max - min + 1)
+    countsLength := len(counts)
+    // 统计每个整数出现的次数
+    for i := 0; i < length; i++ {
+        counts[cs.elementsInt[i] - min]++
     }
     // 累加次数
     for i := 1; i < countsLength; i++ {
@@ -43,11 +94,11 @@ func (cs *CountingSort) SortInt(elements []int) {
     // 从后往前遍历元素，将它放到有序数组中的合适位置
     newElements := make([]int, length)
     for i := length - 1; i >= 0; i-- {
-        counts[cs.elements[i] - min]--
-        newElements[counts[cs.elements[i] - min]] = cs.elements[i]
+        counts[cs.elementsInt[i] - min]--
+        newElements[counts[cs.elementsInt[i] - min]] = cs.elementsInt[i]
     }
     for i := 0; i < length; i++ {
-        cs.elements[i] =  newElements[i]
+        cs.elementsInt[i] =  newElements[i]
     }
 }
 
@@ -55,21 +106,21 @@ func (cs *CountingSort) SortInt(elements []int) {
 // 最简单的实现
 // 不稳定、内存浪费极大、无法给负数排序
 func (cs *CountingSort) SortInt1(elements []int) {
-    cs.elements = elements
+    cs.elementsInt = elements
     length := len(cs.elements)
-    max := cs.elements[0]
+    max := cs.elementsInt[0]
     
     // 找出最大值
     for i := 1; i < length; i++ {
-        if cs.elements[i] > max {
-            max = cs.elements[i]
+        if cs.elementsInt[i] > max {
+            max = cs.elementsInt[i]
         }
     }
     
     // 开辟内存空间，存储每个整数出现的次数
     counts := make([]int, max + 1)
     for i := 0; i < length; i++ {
-        counts[cs.elements[i]]++
+        counts[cs.elementsInt[i]]++
     }
     
     // 根据整数的出现次数，对整数进行排序
@@ -77,7 +128,7 @@ func (cs *CountingSort) SortInt1(elements []int) {
     countsLength := len(counts)
     for i := 0; i < countsLength; i++ {
         for ;counts[i] > 0; counts[i]-- {
-            cs.elements[index] = i
+            cs.elementsInt[index] = i
             index++
         }
     }
--- sorts/counting_sort_test.go
+++ sorts/counting_sort_test.go
@@ -1,15 +1,43 @@
 package sorts
 
 import (
-    "math/rand"
-    "testing"
+	"math/rand"
+	"strconv"
+	"testing"
 )
 
-func TestCountingSort_Sort(t *testing.T) {
+type MyCountSort struct {
+	name string
+	score int
+}
+
+func NewMyCountSort(name string, score int) MyCountSort {
+	return MyCountSort{
+		name: name,
+		score: score,
+	}
+}
+
+func (mcs MyCountSort) GetNum() int {
+	return mcs.score
+}
+
+func TestCountingSort_SortInt(t *testing.T) {
     cs := NewCountingSort()
     data := rand.Perm(100000000)
     //t.Log(data)
     cs.SortInt(data)
     //t.Log(data)
     t.Log(IntsAreAsSorted(data))
+}
+
+func TestCountingSort_Sort(t *testing.T) {
+	cs := NewCountingSort()
+	data := make(Elements, 10)
+	for i := 0; i < 10; i++ {
+		data[i] = NewMyCountSort("xinyulu" + strconv.Itoa(i), rand.Intn(10))
+	}
+	t.Log(data)
+	cs.Sort(data)
+	t.Log(data)
 }
