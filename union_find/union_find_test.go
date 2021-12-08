19bf0d060b4a1af61aa66753849e11a4fd2bb499
xinyulu3344 <xinyulu3344@163.com>
2021-12-09 00:53:56 +0800

    feat: 通用并查集

--- union_find/union_find_test.go
+++ union_find/union_find_test.go
@@ -1,6 +1,8 @@
 package union_find
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewUnionFindQF(t *testing.T) {
     uf := NewUnionFindQF(12)
@@ -38,4 +40,48 @@ func TestNewUnionFindQU(t *testing.T) {
     t.Log(uf.IsSame(2, 7))
     uf.Union(4, 6)
     t.Log(uf.IsSame(2, 7))
+}
+
+type Student struct {
+	name string
+	age int
+}
+
+func NewStudent(name string, age int) *Student {
+	return &Student{
+		name: name,
+		age: age,
+	}
+}
+
+func (s *Student) IsEqual(v Value) bool {
+	stu := v.(*Student)
+	if s.name == stu.name && s.age == stu.age {
+		return true
+	} else {
+		return false
+	}
+}
+
+func TestGenericUnionFind(t *testing.T) {
+	uf := NewGenericUnionFind()
+	stu1 := NewStudent("jack", 1)
+	stu2 := NewStudent("rose", 2)
+	stu3 := NewStudent("jack", 3)
+	stu4 := NewStudent("rose", 4)
+	uf.MakeSet(stu1)
+	uf.MakeSet(stu2)
+	uf.MakeSet(stu3)
+	uf.MakeSet(stu4)
+	
+	uf.Union(stu1, stu2)
+	uf.Union(stu3, stu4)
+	uf.Union(stu1, stu4)
+
+	t.Log(uf.IsSame(stu1, stu2))
+	t.Log(uf.IsSame(stu1, stu3))
+	t.Log(uf.IsSame(stu1, stu4))
+	t.Log(uf.IsSame(stu2, stu3))
+	t.Log(uf.IsSame(stu2, stu4))
+	t.Log(uf.IsSame(stu3, stu4))
 }
