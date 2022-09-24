0cdae2ac026cf5ef2543442c2650ff15d7881da5
xinyulu <xinyulu3344@163.com>
2022-09-24 23:37:34 +0800

    feat: 单向循环链表

---
+++ single_circle_linked_list/single_circle_linked_list.go
@@ -0,0 +1,170 @@
+package single_circle_linked_list
+
+import (
+    "fmt"
+)
+
+const (
+    ELEMENT_NOT_FOUND = -1
+)
+
+type E interface {
+    Equal(e E) bool
+}
+
+type node struct {
+    element E
+    next    *node
+}
+
+type ILinkedList interface {
+    Size() int
+    IsEmpty() bool
+    Contains(e E) bool
+    Append(e E)
+    Get(index int) E
+    Set(index int, e E) E
+    Add(index int, e E)
+    Remove(index int) E
+    IndexOf(e E) int
+    Clear()
+}
+
+type CircleLinkedList struct {
+    size int
+    root *node
+}
+
+func NewCircleLinkedList() *CircleLinkedList {
+    return &CircleLinkedList{}
+}
+
+func (l *CircleLinkedList) Size() int {
+    return l.size
+}
+
+func (l *CircleLinkedList) IsEmpty() bool {
+    return l.size == 0
+}
+
+func (l *CircleLinkedList) Contains(e E) bool {
+    return l.IndexOf(e) != ELEMENT_NOT_FOUND
+}
+
+func (l *CircleLinkedList) Append(e E) {
+    l.Add(l.size, e)
+}
+
+func (l *CircleLinkedList) Get(index int) E {
+    return l.getNodeByIndex(index).element
+}
+
+func (l *CircleLinkedList) Set(index int, e E) E {
+    n := l.getNodeByIndex(index)
+    old := n.element
+    n.element = e
+    return old
+}
+
+func (l *CircleLinkedList) Add(index int, e E) {
+    l.rangeCheckForAdd(index)
+    if index == 0 {
+        l.root = &node{element: e, next: l.root}
+        var last *node
+        if l.size == 0 { // 如果是空链表, 让last指向root
+            last = l.root
+        } else {
+            // 获取最后一个节点
+            last = l.getNodeByIndex(l.size - 1).next
+        }
+        last.next = l.root
+    } else {
+        prev := l.getNodeByIndex(index - 1)
+        prev.next = &node{element: e, next: prev.next}
+    }
+    l.size++
+}
+
+func (l *CircleLinkedList) Remove(index int) E {
+    l.rangeCheck(index)
+    n := l.root
+    if index == 0 {
+        if l.size == 1 {
+            l.root = nil
+        } else {
+            last := l.getNodeByIndex(l.size - 1)
+            l.root = l.root.next
+            last.next = l.root
+        }
+    } else {
+        prev := l.getNodeByIndex(index - 1)
+        n = prev.next
+        prev.next = prev.next.next
+    }
+    l.size--
+    return n.element
+}
+
+func (l *CircleLinkedList) IndexOf(e E) int {
+    n := l.root
+    if e == nil {
+        for i := 0; i < l.size; i++ {
+            if n.element == nil {
+                return i
+            }
+            n = n.next
+        }
+    } else {
+        for i := 0; i < l.size; i++ {
+            if e.Equal(n.element) {
+                return i
+            }
+            n = n.next
+        }
+    }
+    return ELEMENT_NOT_FOUND
+}
+
+func (l *CircleLinkedList) Clear() {
+    l.size = 0
+    l.root = nil
+}
+
+func (l *CircleLinkedList) String() string {
+    alStr := "["
+    n := l.root
+    for i := 0; i < l.size; i++ {
+        if i != 0 {
+            alStr = alStr + ", "
+        }
+        alStr = alStr + fmt.Sprintf("%#v", n.element)
+        n = n.next
+    }
+    return alStr + "]"
+}
+
+// 获取index位置对应的节点对象
+func (l *CircleLinkedList) getNodeByIndex(index int) *node {
+    l.rangeCheck(index)
+    n := l.root
+    for i := 0; i < index; i++ {
+        n = n.next
+    }
+    return n
+}
+
+func (l *CircleLinkedList) outOfBounds(index int) {
+    panic(fmt.Sprintf("Index: %d Size: %d", index, l.size))
+}
+
+func (l *CircleLinkedList) rangeCheck(index int) {
+    if index < 0 || index >= l.size {
+        l.outOfBounds(index)
+    }
+}
+
+func (l *CircleLinkedList) rangeCheckForAdd(index int) {
+    if index < 0 || index > l.size {
+        l.outOfBounds(index)
+    }
+}
---
+++ single_circle_linked_list/single_circle_linked_list_test.go
@@ -0,0 +1,83 @@
+package single_circle_linked_list
+
+import (
+    "testing"
+)
+
+type Int int
+
+func (i Int) Equal(j E) bool {
+    return i == j.(Int)
+}
+
+type Str string
+
+func (s Str) Equal(j E) bool {
+    return s == j.(Str)
+}
+
+type Persion struct {
+    Name string
+    Age  int
+}
+
+func NewPersion(name string, age int) Persion {
+    return Persion{
+        Name: name,
+        Age:  age,
+    }
+}
+
+func (p Persion) Equal(j E) bool {
+    if persion, ok := j.(Persion); ok {
+        return p.Age == persion.Age
+    }
+    return false
+}
+
+func TestCircleLinkedList(t *testing.T) {
+    list := NewCircleLinkedList()
+    list.Append(Int(1))
+    list.Append(Int(2))
+    list.Add(0, Int(3))
+    list.Append(Int(4))
+    list.Add(list.Size(), Int(5))
+    list.Remove(1)
+    
+    if list.Size() != 4 {
+        t.Errorf("%v list.Size() = %v != 5\n", list, list.Size())
+    }
+    
+    if !list.Get(0).Equal(Int(3)) || !list.Get(1).Equal(Int(2)) || !list.Get(2).Equal(Int(4)) || !list.Get(3).Equal(Int(5)) {
+        t.Errorf("list.Get() err\n")
+    }
+    
+    list.Set(0, Int(6))
+    list.Set(list.Size()-1, Int(7))
+    list.Set(2, Int(8))
+    
+    if !list.Get(0).Equal(Int(6)) || !list.Get(1).Equal(Int(2)) || !list.Get(2).Equal(Int(8)) || !list.Get(3).Equal(Int(7)) {
+        t.Errorf("list.Get() err\n")
+    }
+    
+    list.Clear()
+    
+    if list.Size() != 0 {
+        t.Errorf("list.Clear(), Size err")
+    }
+}
+
+func TestCircleLinkedList1(t *testing.T) {
+    list := NewCircleLinkedList()
+    list.Append(Int(11))
+    list.Append(Int(22))
+    list.Append(Int(33))
+    list.Append(Int(44))
+    list.Add(0, Int(55))           // [55, 11, 22, 33, 44]
+    list.Add(2, Int(66))           // [55, 11, 66, 22, 33, 44]
+    list.Add(list.Size(), Int(77)) // [55, 11, 66, 22, 33, 44, 77]
+    
+    list.Remove(0)
+    list.Remove(2)
+    list.Remove(list.Size() - 1)
+}
