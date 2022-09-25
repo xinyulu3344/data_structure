dc39b0d7d864aab016c4a6ef41d09f5301c1acfb
xinyulu <xinyulu3344@163.com>
2022-09-25 23:44:54 +0800

    docs: 注释

--- array_list/array_list.go
+++ array_list/array_list.go
@@ -31,6 +31,8 @@ type ArrayList struct {
     elements []Item
 }
 
+// NewArrayListWithCap 创建一个动态数组对象
+// capacity 动态数组起始容量， 如果小于DEFAULT_CAPACITY(10)，则设置为DEFAULT_CAPACITY
 func NewArrayListWithCap(capacity int) *ArrayList {
     if capacity < 0 {
         capacity = DEFAULT_CAPACITY
@@ -41,33 +43,38 @@ func NewArrayListWithCap(capacity int) *ArrayList {
     }
 }
 
+// NewArrayList 创建一个容量为DEFAULT_CAPACITY的动态数组
 func NewArrayList() *ArrayList {
     return NewArrayListWithCap(DEFAULT_CAPACITY)
 }
 
+// Size 获取动态数组的元素数量
 func (al *ArrayList) Size() int {
     return al.size
 }
 
+// IsEmpty 判断动态数组是否为空数组
 func (al *ArrayList) IsEmpty() bool {
     return al.size == 0
 }
 
-// Contains
-// 判断数组是否包含元素
+// Contains 判断动态数组是否包含指定元素
 func (al *ArrayList) Contains(e Item) bool {
     return al.IndexOf(e) != ELEMENT_NOT_FOUND
 }
 
+// Append 向动态数组最后位置插入元素
 func (al *ArrayList) Append(e Item) {
     al.Add(al.size, e)
 }
 
+// Get 获取指定索引位置的元素
 func (al *ArrayList) Get(index int) Item {
     al.rangeCheck(index)
     return al.elements[index]
 }
 
+// Set 设置指定索引位置的元素
 func (al *ArrayList) Set(index int, e Item) Item {
     al.rangeCheck(index)
     old := al.elements[index]
@@ -75,6 +82,7 @@ func (al *ArrayList) Set(index int, e Item) Item {
     return old
 }
 
+// Add 向指定索引位置插入元素
 func (al *ArrayList) Add(index int, e Item) {
     al.rangeCheckForAdd(index)
     al.ensureCapacity(al.size + 1)
@@ -85,6 +93,7 @@ func (al *ArrayList) Add(index int, e Item) {
     al.size++
 }
 
+// Remove 移除指定索引位置的元素
 func (al *ArrayList) Remove(index int) Item {
     al.rangeCheck(index)
     old := al.Get(index)
@@ -97,8 +106,7 @@ func (al *ArrayList) Remove(index int) Item {
     return old
 }
 
-// IndexOf
-// 返回元素在数组中的索引，若不存在，则返回-1
+// IndexOf 返回元素在数组中的索引，若不存在，则返回-1
 func (al *ArrayList) IndexOf(e Item) int {
     if e == nil {
         for i := 0; i < al.size; i++ {
@@ -116,6 +124,7 @@ func (al *ArrayList) IndexOf(e Item) int {
     return ELEMENT_NOT_FOUND
 }
 
+// Clear 清空数组，并缩容
 func (al *ArrayList) Clear() {
     for i := 0; i < al.size; i++ {
         al.elements[i] = nil
@@ -126,6 +135,7 @@ func (al *ArrayList) Clear() {
     }
 }
 
+// 扩容数组，将数组容量扩容为原来1.5倍
 func (al *ArrayList) ensureCapacity(capacity int) {
     oldCapacity := len(al.elements)
     if oldCapacity >= capacity {
@@ -140,6 +150,7 @@ func (al *ArrayList) ensureCapacity(capacity int) {
     al.elements = newElements
 }
 
+// 缩容数组，将数组缩容为原来一半
 func (al *ArrayList) trim() {
     oldCapacity := len(al.elements)
     if al.size >= (oldCapacity>>1) || oldCapacity <= DEFAULT_CAPACITY {
@@ -154,22 +165,26 @@ func (al *ArrayList) trim() {
     al.elements = newElements
 }
 
+// 索引越界panic
 func (al *ArrayList) outOfBounds(index int) {
     panic(fmt.Sprintf("Index: %d Size: %d", index, al.size))
 }
 
+// 索引越界检查
 func (al *ArrayList) rangeCheck(index int) {
     if index < 0 || index >= al.size {
         al.outOfBounds(index)
     }
 }
 
+// 针对新增元素的索引越界检查
 func (al *ArrayList) rangeCheckForAdd(index int) {
     if index < 0 || index > al.size {
         al.outOfBounds(index)
     }
 }
 
+// 打印数组时的字符串表示
 // [xx, xx, xx, xx]
 func (al *ArrayList) String() string {
     alStr := "["
--- single_linked_list/single_linked_list.go
+++ single_linked_list/single_linked_list.go
@@ -9,13 +9,16 @@ const (
     ELEMENT_NOT_FOUND = -1
 )
 
+// E 链表节点中的元素
 type E interface {
     Equal(e E) bool
 }
 
+// 链表节点
 type node struct {
     element E
-    next    *node
+    // 链表下一个节点指针
+    next *node
 }
 
 type ILinkedList interface {
@@ -31,35 +34,46 @@ type ILinkedList interface {
     Clear()
 }
 
+// SingleLinkedList 链表结构
 type SingleLinkedList struct {
+    // 链表中节点数量
     size int
+    
+    // 链表头指针
     root *node
 }
 
+// NewSingleLinkedList 返回一个链表对象
 func NewSingleLinkedList() *SingleLinkedList {
     return &SingleLinkedList{}
 }
 
+// Size 获取链表中元素个数
 func (l *SingleLinkedList) Size() int {
     return l.size
 }
 
+// IsEmpty 判断链表是否为空
 func (l *SingleLinkedList) IsEmpty() bool {
     return l.size == 0
 }
 
+// Contains 判断链表中是否包含指定元素
 func (l *SingleLinkedList) Contains(e E) bool {
     return l.IndexOf(e) != ELEMENT_NOT_FOUND
 }
 
+// Append 向链表最后位置添加元素
 func (l *SingleLinkedList) Append(e E) {
     l.Add(l.size, e)
 }
 
+// Get 获取指定位置的元素
 func (l *SingleLinkedList) Get(index int) E {
     return l.getNodeByIndex(index).element
 }
 
+// Set 设置指定位置的元素
 func (l *SingleLinkedList) Set(index int, e E) E {
     n := l.getNodeByIndex(index)
     old := n.element
@@ -67,6 +81,7 @@ func (l *SingleLinkedList) Set(index int, e E) E {
     return old
 }
 
+// Add 向指定位置插入元素
 func (l *SingleLinkedList) Add(index int, e E) {
     l.rangeCheckForAdd(index)
     if index == 0 {
@@ -78,6 +93,7 @@ func (l *SingleLinkedList) Add(index int, e E) {
     l.size++
 }
 
+// Remove 删除指定位置的元素
 func (l *SingleLinkedList) Remove(index int) E {
     l.rangeCheck(index)
     n := l.root
@@ -92,6 +108,7 @@ func (l *SingleLinkedList) Remove(index int) E {
     return n.element
 }
 
+// IndexOf 获取指定元素所在的位置
 func (l *SingleLinkedList) IndexOf(e E) int {
     n := l.root
     if e == nil {
@@ -112,6 +129,7 @@ func (l *SingleLinkedList) IndexOf(e E) int {
     return ELEMENT_NOT_FOUND
 }
 
+// Clear 清空链表
 func (l *SingleLinkedList) Clear() {
     l.size = 0
     l.root = nil
